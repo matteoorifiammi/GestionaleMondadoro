fe402fdb4956485b2a49995dbd3fd4cf248768f9
matteoorifiammi <72445433+matteoorifiammi@users.noreply.github.com>
2022-03-03 10:13:11 +0100

    working with files

--- gestionale_mondadoro/Pages/magazzino.cshtml.cs
+++ gestionale_mondadoro/Pages/magazzino.cshtml.cs
@@ -3,4 +3,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace gestionale_mondadoro.Pages
 {
+    //public class StorageController : Controller
+    //{
+    //    public ActionResult Magazzino()
+    //    {
+    //        string[] texts = System.IO.File.ReadAllLines("/gestionale_mondadoro/Pages/storage.txt");
+
+    //        ViewBag.texts = texts;
+    //        return View();
+    //    }
+    //}
 }
