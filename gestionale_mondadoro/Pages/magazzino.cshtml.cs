937b0bf02bd4834356a69003e49fd1ebd51bd460
Angelo Pavan <angelopavan.dev@gmail.com>
2022-03-04 19:17:21 +0100

    Added .txt file source

--- gestionale_mondadoro/Pages/magazzino.cshtml.cs
+++ gestionale_mondadoro/Pages/magazzino.cshtml.cs
@@ -4,34 +4,34 @@ using System.Text.Json;
 
 namespace gestionale_mondadoro.Pages
 {
-    public class StorageItem
-    {
-        public string Name { get; set; }
-        public int Qnty { get; set; }
-    }
+    //public class StorageItem
+    //{
+    //    public string Name { get; set; }
+    //    public int Qnty { get; set; }
+    //}
 
-    public class Program
-    {
-        public static void Main()
-        {
-            var storageItem = new StorageItem
-            {
-                Name = "test",
-                Qnty = 1
-            };
+    //public class Program
+    //{
+    //    public static void Main()
+    //    {
+    //        var storageItem = new StorageItem
+    //        {
+    //            Name = "test",
+    //            Qnty = 1
+    //        };
 
-            //string fileName = "StorageItem.json";
-            //using FileStream createStream = File.Create(fileName);
-            //await JsonSerializer.SerializeAsync(createStream, storageItem);
-            //await createStream.DisposeAsync();
+    //        //string fileName = "StorageItem.json";
+    //        //using FileStream createStream = File.Create(fileName);
+    //        //await JsonSerializer.SerializeAsync(createStream, storageItem);
+    //        //await createStream.DisposeAsync();
 
-            //Console.WriteLine(File.ReadAllText(fileName));
+    //        //Console.WriteLine(File.ReadAllText(fileName));
 
-            var options = new JsonSerializerOptions { WriteIndented = true };
-            string jsonString = JsonSerializer.Serialize(storageItem, options);
+    //        var options = new JsonSerializerOptions { WriteIndented = true };
+    //        string jsonString = JsonSerializer.Serialize(storageItem, options);
 
-            Console.WriteLine(jsonString);
-        }
+    //        Console.WriteLine(jsonString);
+    //    }
         
-    }
+    //}
 }
